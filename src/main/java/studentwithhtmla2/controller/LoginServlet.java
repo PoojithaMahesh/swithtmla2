0e983d8387e620970ad5805d7a30f5de18c26cd1
unknown <poojithadarshan2131@gmail.com>
2024-01-12 15:25:02 +0530

    final

---
+++ src/main/java/studentwithhtmla2/controller/LoginServlet.java
@@ -0,0 +1,17 @@
+package studentwithhtmla2.controller;
+
+import java.io.IOException;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class LoginServlet extends  GenericServlet{
+
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		System.out.println("Reached LOGIN SERVLET");
+	}
+
+}
---
+++ src/main/java/studentwithhtmla2/dao/StudentDao.java
@@ -0,0 +1,29 @@
+package studentwithhtmla2.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+import studentwithhtmla2.dto.Student;
+
+public class StudentDao {
+
+	public EntityManager getEntityManager() {
+		return Persistence.createEntityManagerFactory("vinod").createEntityManager();
+	}
+	
+	public void saveStudent(Student  student) {
+		EntityManager entityManager=getEntityManager();
+		EntityTransaction entityTransaction=entityManager.getTransaction();
+		entityTransaction.begin();
+		entityManager.persist(student);
+		entityTransaction.commit();
+	}
+	
+	
+	
+	
+	
+	
+	
+}
